99821d1a60fed85e856e980ca3231f89ae990cf9
islemchatti <islemzommit@gmail.com>
2019-07-07 15:18:37 +0100

    hello wael

--- js/main.js
+++ js/main.js
@@ -1,29 +1,57 @@
 function insert(num){
     document.getElementById("input").value=document.getElementById("input").value + num;
+   
 }
 function operation(p){
-var x=document.getElementById("input").value;
-alert(x);
-operateur=p;
-alert(p);
+document.getElementById("input2").value =document.getElementById("input").value;
+document.getElementById("input3").value=p;
 document.getElementById("input").value="";
-
-
+return operateur;
 }
 function somme(x,y){
-    document.getElementById("input").value=x+y; 
+    return x + y; 
+}
+function diff(x,y){
+    return x-y; 
 }
 function multi(x,y){
-    document.getElementById("input").value=x*y; 
+    return x*y; 
 }
 function div(x,y){
-    document.getElementById("input").value=x/y; 
+    return x/y; 
 }
 function mod(x,y){
-    document.getElementById("input").value=x%y; 
+    return x%y; 
 }
 function egale(){
-    var y=document.getElementById("input").value;
-    alert(y);
-    operation();
+    var y=0;
+    var x=0;
+     y=document.getElementById("input").value;
+     x=document.getElementById("input2").value;
+    var s=document.getElementById("input3").value;
+     if(s=='+'){
+        document.getElementById("input").value=somme(x,y);
+     }
+     if(s=='-'){
+        document.getElementById("input").value=diff(x,y)
+     }
+     if(s=='/'){
+        document.getElementById("input").value=div(x,y);
+     }
+     if(s=='*'){
+        document.getElementById("input").value=multi(x,y);
+     }
+     if(s=='%'){
+        document.getElementById("input").value=mod(x,y);
+     }
+     var res=document.getElementById("input").value;
+     document.getElementById("results").value=document.getElementById("results").value+x+s+y+'='+ res;
+}
+function reset(){
+    document.getElementById("input").value="";
+    document.getElementById("input2").value="";
+    document.getElementById("input3").value="";
+}
+function clear(){
+    document.getElementById("results").value="";
 }
