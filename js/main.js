7d5cb20d92d31f96704f20d410192df29513874b
NoNameAB <48261117+NoNameAB@users.noreply.github.com>
2019-07-09 09:58:07 +0100

    Update main.js
    Fixed a bug so now you don't need to type deg after a number for the gradient

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function change1(){
     var color3 = document.getElementById("color3").value ;
     var color4 = document.getElementById("color4").value ;
     var deg1 = document.getElementById("number1").value;
-    var gradient1 = "linear-gradient("+deg1+ ","+  color3  + "," +  color4  +")";
+    var gradient1 = "linear-gradient("+deg1+ "deg,"+  color3  + "," +  color4  +")";
     document.body.style.backgroundImage= gradient1;
 }
 
@@ -11,7 +11,7 @@ function change(){
     var color1 = document.getElementById("color1").value ;
     var color2 = document.getElementById("color2").value ;
     var deg = document.getElementById("number").value;
-    var gradient = "linear-gradient("+deg+ ","+  color1  + "," +  color2  +")";
+    var gradient = "linear-gradient("+deg+ "deg,"+  color1  + "," +  color2  +")";
     document.getElementById("changing").style.backgroundImage= gradient;
     document.getElementById("changing1").style.backgroundImage= gradient;
     document.getElementById("changing2").style.backgroundImage= gradient;
